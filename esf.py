48f57bd2bf17b3bc5229be43ee953b77515d32ad
Zakaria EL BAZI <zakaria.elbazi@exceleratesystems.net>
2019-12-27 03:29:22 +0100

    Update esf.py

--- esf.py
+++ esf.py
@@ -13,6 +13,7 @@ import requests
 from pybinaryedge import BinaryEdge
 from urllib.parse import urlparse
 from datetime import datetime
+import xlsxwriter
 __version__ = "1.0.1"
 SHODAN_API_KEY =""
 BINARYEDGE_API_KEY = ''
@@ -43,13 +44,35 @@ def reverse_dns(ip):
             organization = element['ssl']['cert']['subject']['CN']
             break
     return hoster,organization
+
+#export data to excel :
+def create_workbook(filename):
+    name = str(filename) + ".xlsx"
+    return xlsxwriter.Workbook(name)
+#separate shodan and binary edge data in different sheets :
+def create_worksheet(workbook,worksheet_name):
+    worksheet = workbook.add_worksheet(str(worksheet_name))
+    bold = workbook.add_format({'bold': 1})
+    worksheet.set_column(1, 1, 15)
+    worksheet.write('A1', 'Host IP', bold)
+    worksheet.write('B1', 'Port number', bold)
+    worksheet.write('C1', 'Source', bold)
+    worksheet.write('D1', 'Country', bold)
+    worksheet.write('E1', 'Cluster name', bold)
+    worksheet.write('F1', 'Hosting provider', bold)
+    worksheet.write('G1', 'organization', bold)
+    worksheet.write('H1', 'Number of nodes', bold)
+    worksheet.write('I1', 'Cluster size', bold)
+    worksheet.write('J1', 'Indices', bold)
+    return worksheet
+
 def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-o','--output',dest = "output",help = "Name of the file where results will be stored. Default will be es(timedate).txt",required = False)
     parser.add_argument('-c','--country', dest = "country",help = "The country you want to scan.",required = False)
     parser.add_argument('-k','--keyword',dest = "keyword",help = "add a keyword to your search like a specific indice name",required = False)
     parser.add_argument('-f','--first', dest = "first",help = "the first page to check for binary edge, default will be 1",default= 1,type= int, required = False)
-    parser.add_argument('-l','--last',dest = "last",help = "the last page to check for binary edge, default will be 3",default= 3,type= int,required = False)
+    parser.add_argument('-l','--last',dest = "last",help = "the last page to check for binary edge, default will be 3",default= 2,type= int,required = False)
     parser.add_argument('-s','--shodan',action='store_true',dest="shodan", help = "pull data from shodan")
     parser.add_argument('-b','--be',dest="binaryedge",action='store_true',help = "pull data from binary edge")
     return parser.parse_args()
@@ -81,13 +104,15 @@ def binaryedge_query(query,page):
 
     return req_json['events']
 
-def getHosts_binaryedge(first,last,filename):
+def getHosts_binaryedge(first,last,filename,workbook):
+    worksheet = create_worksheet(workbook,'binaryedge')
     for page in range(first, last) :
         if parse_args().country is not None:
             query = "type:%22elasticsearch%22" +" " + "country:"+'"'+ str(parse_args().country)+'"'
         else:
             query = "type:%22elasticsearch%22" +" "
         elastic_results = binaryedge_query(query,page)
+        row = 1
         for service in elastic_results:
                 #print (service)
                 #print('http://' + service['target']['ip'] + ":" + str(service['target']['port']) + "/_cat/indices")
@@ -96,6 +121,7 @@ def getHosts_binaryedge(first,last,filename):
                 country = service['origin']['country']
                 cluster_name = service['result']['data']['cluster_name']
                 hoster = reverse_dns(host)[0]
+                #hoster = "test"
                 organization = reverse_dns(host)[1]
                 #organization = "test"
                 number_nodes = service['result']['data']['cluster_nodes']
@@ -122,6 +148,17 @@ def getHosts_binaryedge(first,last,filename):
                     print ("cluster size : ",size(sizee))
                 except:
                     print("No indices")
+                worksheet.write(row, 0,host)
+                worksheet.write(row, 1,port_number)
+                worksheet.write(row, 2,"binary edge")
+                worksheet.write(row, 3,country)
+                worksheet.write(row, 4,cluster_name)
+                worksheet.write(row, 5,hoster)
+                worksheet.write(row, 6,organization)
+                worksheet.write(row, 7,number_nodes)
+                worksheet.write(row, 8,str(size(sizee)))
+                worksheet.write(row, 9,str(indices))
+                row = row + 1
                 write( ["host:" + host + "\n", 
                 "Port number :" + port_number+ "\n",
                 "source : binary edge" + "\n", 
@@ -134,8 +171,9 @@ def getHosts_binaryedge(first,last,filename):
                 print(" \n ----------------------------- \n")
 
         break
-def getHosts_shodan(filename):
+def getHosts_shodan(filename,workbook):
     api = shodan.Shodan(SHODAN_API_KEY)
+    worksheet = create_worksheet(workbook,'shodan')
     if parse_args().country is not None:
         query = 'port:9200 json country:'+ '"'+ str(parse_args().country)+'"'
     else:
@@ -143,6 +181,7 @@ def getHosts_shodan(filename):
     try:
         for p in range(1, 150):
             results = api.search(query, page=p)
+            row = 1
             for result in results['matches']:
                 #print(result)
                 host = str(result['ip_str'])
@@ -176,6 +215,17 @@ def getHosts_shodan(filename):
                     " number of nodes : "+ str(number_nodes)+ "\n",
                     "size of the cluster :"  + str(size(sizee)) + "\n",
                     "indices" + data[data.find('Elastic Indices'):]," \n ----------------------------- \n"], filename)
+                    worksheet.write(row, 0,host)
+                    worksheet.write(row, 1,port_number)
+                    worksheet.write(row, 2,"shodan")
+                    worksheet.write(row, 3,country)
+                    worksheet.write(row, 4,cluster_name)
+                    worksheet.write(row, 5,"")
+                    worksheet.write(row, 6,organization)
+                    worksheet.write(row, 7,number_nodes)
+                    worksheet.write(row, 8,str(size(sizee)))
+                    worksheet.write(row, 9,str(data[data.find('Elastic Indices'):]))
+                    row = row + 1
                 except KeyError as e:
                     print (e)
                     pass
@@ -192,14 +242,15 @@ def main():
     last = parse_args().last
     shodan = parse_args().shodan
     be = parse_args().binaryedge
-    
+    wk = create_workbook(filename)
     if ((not shodan) and (not be)) :
         print("Please specify a data source by adding -s and/or -b")
         sys.exit()
     if shodan :
-        getHosts_shodan(filename)
+        getHosts_shodan(filename,wk)
+        #to_excel()
     if be:
-        getHosts_binaryedge(first,last,filename)
-   
+        getHosts_binaryedge(first,last,filename,wk)
+    wk.close()
 if __name__ == '__main__':
     main()
